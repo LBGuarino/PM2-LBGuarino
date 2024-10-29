c263eea838ad7ca78b61c5806074e81c9a52fe02
lbguarino <lbelen2014@gmail.com>
2024-10-29 20:37:27 +0100

    Hito Async

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,10 +1,14 @@
 const {Movies, Repository} = require("./classes");
 const renderMovies = require("./renderCards");
+const axios = require("axios");
 
 const repository = new Repository();
 
-  $.get("https://students-api.up.railway.app/movies", (data) => {
-    data.forEach((movieData) => {
+const fetchData = async () => {
+  try {
+    console.log("Estamos intentando hacer una solicitud");
+    const data = await axios.get("https://students-api.up.railway.app/movies");
+    (data.data).forEach((movieData) => {
       const movie = new Movies(
         movieData.title,
         movieData.year,
@@ -17,8 +21,9 @@ const repository = new Repository();
       repository.addMovie(movie);
     });
     renderMovies(repository);
-  });
-  
-
-
-
+  } catch (err) {
+    console.log("Tuvimos un error. Estamos en el bloque catch");
+    console.log(err.message);
+  };
+};
+fetchData();
